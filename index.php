163a7ac352a6e1a9e0249a43f51758510a40089b
danialfayyad <ntchelmed@gmail.com>
2024-09-25 13:43:58 +0700

    innit commit

---
+++ index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Web Admin</title>
+
+    <!-- Import Icon Unicons -->
+    <link rel="stylesheet" href="https://unicons.iconscout.com/release/v4.0.8/css/line.css">
+
+    <!-- Import Icon Boxicon -->
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+    
+    <!-- Link ke CSS Sidebar dan Home Section -->
+    <link rel="stylesheet" href="css/sidebar.css">
+    <link rel="stylesheet" href="css/home-section.css">
+</head>
+<body>
+
+    <!-- Sidebar -->
+    <nav>
+        <?php include 'sidebar/sidebar.php'; ?>
+    </nav>
+    
+    <!-- Home Section -->
+    <section class="home-section">
+        <header>
+            <div class="home-logo">
+                <i class="uil uil-estate"></i>
+                <span class="dashboard">Dashboard</span>
+            </div>
+
+            <div class="search-box">
+                <i class='bx bx-search'></i>
+                <input type="text" placeholder="Cari...">
+            </div>
+
+            <div class="profile">
+                <img src="images/logo.jpg" alt="Profile Picture">
+                <span class="admin_name">Naraya</span>
+            </div>
+        </header>
+    </section>
+
+</body>
+</html>
---
+++ sidebar/sidebar.php
@@ -0,0 +1,64 @@
+
+   
+    <link rel="stylesheet" href="css/sidebar.css">
+
+    <link rel="stylesheet" href="https://unicons.iconscout.com/release/v4.0.8/css/line.css">
+
+    <nav>
+        <div class="logo-name">
+            <div class="logo-image">
+                <img src="images/logo.jpg" alt="">
+            </div>
+            <span class="logo_name">Mechaban</span>
+        </div>
+
+        <div class="menu-items">
+            <ul class="nav-links">
+                <li><a href="#">
+                    <i class="uil uil-estate"></i>
+                    <span class="link-name">Dasboard</span>
+                </a></li>
+                <li><a href="#">
+                    <i class="uil uil-estate"></i>
+                    <span class="link-name">Pelanggan</span>
+                </a></li>
+                <li><a href="#">
+                    <i class="uil uil-book"></i>
+                    <span class="link-name">Reservasi</span>
+                </a></li>
+                <li><a href="#">
+                    <i class="uil uil-estate"></i>
+                    <span class="link-name">Servis Kendaraan</span>
+                </a></li>
+                <li><a href="#">
+                    <i class="uil uil-estate"></i>
+                    <span class="link-name">Montir</span>
+                </a></li>
+                <li><a href="#">
+                    <i class="uil uil-estate"></i>
+                    <span class="link-name">Sparepart</span>
+                </a></li>
+                <li><a href="#">
+                    <i class="uil uil-file-medical-alt"></i>
+                    <span class="link-name">Laporan</span>
+                </a></li>
+            </ul>
+
+            <ul class="logout-mode">
+                <li><a href="#">
+                    <i class="uil uil-signout"></i>
+                    <span class="link-name">Logout</span>
+                </a></li>
+                
+                <!-- <li>
+                    <a href="#">
+                    <i class="uil uil-moon"></i> 
+                    <span class="link-name">Dark Mode</span>
+                    </a>
+                </li> -->
+            </ul>
+        </div>
+    </nav>
+    
+</body>
+</html>
